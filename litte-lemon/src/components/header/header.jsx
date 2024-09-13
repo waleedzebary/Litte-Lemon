d0ddbac1c950af7695ea7af74b3467cf364eb1e7
unknown <waleedzebary321@gmail.com>
2024-09-13 07:15:05 +0300

    create header component

---
+++ litte-lemon/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import logo from '../../images/logo.jpg'
+
+const Header = () => {
+    return (
+        <>
+      <header>
+        <img src={logo} alt="Little Lemon Logo" />
+      </header>
+      </>
+    );
+  }
+
+export default Header;
