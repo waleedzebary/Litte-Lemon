f1dd17c26c0419e9e09e6e824cad3bd4b74e856e
unknown <waleedzebary321@gmail.com>
2024-09-13 07:15:45 +0300

    make some changes to the navbar

--- litte-lemon/src/components/navbar/navbar.jsx
+++ litte-lemon/src/components/navbar/navbar.jsx
@@ -1,26 +1,18 @@
-import logo from '../../images/logo.jpg';
-import './navbar.css';
-
-const Navbar = () => {
+const Nav = () => {
     return (
         <>
-            <nav>
-                <div className='image'>
-                    <img className='img' src={logo} alt="Logo" />
-                </div>
-                <div className="box">
-                    <ul className="navbar">
-                        <li className="text-wrapper"><a href="/">Home</a></li>
-                        <li className="div"><a href="About">About</a></li>
-                        <li className="text-wrapper-2"><a href="Menu">Menu</a></li>
-                        <li className="text-wrapper-3"><a href="Reservations">Reservations</a></li>
-                        <li className="text-wrapper-4"><a href="Order online">Order online</a></li>
-                        <li className="text-wrapper-5"><a href="Login">Login</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    )
-}
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#menu">Menu</a></li>
+          <li><a href="#reservations">Reservations</a></li>
+          <li><a href="#order">Order online</a></li>
+          <li><a href="#login">Login</a></li>
+        </ul>
+      </nav>
+      </>
+    );
+  }
 
-export default Navbar;
+export default Nav;
