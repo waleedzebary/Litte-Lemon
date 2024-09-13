bfc00444ebe8a8bff265e5a59c572aa2da6dfd89
unknown <waleedzebary321@gmail.com>
2024-09-13 07:16:25 +0300

    updates

--- litte-lemon/src/App.js
+++ litte-lemon/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import Header from './components/header/header';
+import Navbar from './components/navbar/navbar';
+import Main from './components/main/main';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header></Header>
+      <Navbar></Navbar>
+      <Main></Main>
+      <Footer></Footer>
+    </>
   );
 }
 
